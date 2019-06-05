24443e16e842628396f1044ceb59e90aecf210ef
Nuno Carriço <nuno192009@live.com.pt>
2019-06-05 16:35:08 +0100

    Beans

---
+++ FrontEnd/src/java/src/SignUp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package src;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author nuno1
+ */
+@ManagedBean
+@RequestScoped
+public class SignUp {
+
+    /**
+     * Creates a new instance of SignUp
+     */
+    public SignUp() {
+    }
+    
+}
